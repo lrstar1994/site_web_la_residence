34af55cfed6480a23de978848f6b477279c24182
santatraM <santatryn@gmail.com>
2026-08-18 15:55:10 +0300

    correction texte

--- src/components/home/HomeHero.tsx
+++ src/components/home/HomeHero.tsx
@@ -19,7 +19,7 @@ export function HomeHero({
     <header className="hero" id="home-hero" role="banner">
       <div className="hero-image-container">
         <Image
-          src="/ChatGPT Image 2 juin 2026, 09_05_48.png"
+          src="/couverture-acceuil.png"
           alt={imageAlt}
           fill
           priority
